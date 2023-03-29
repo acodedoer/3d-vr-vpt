bc6e3ad1931531e3be23c62b155ba0acc9348ece
acodedoer <misbahu.s.z@gmail.com>
2023-03-29 01:49:40 +0100

    pickups on tiles

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,8 +1,10 @@
+import { PickupModel } from './PickupModel';
 import { TileModel } from './TileModel'
 
 const Tile = (props) => {
     return(
         <mesh>
+          <PickupModel position={[props.position[0],props.position[1]+2,props.position[2]]} scale={[0.5,0.5,0.5]}/>
           <TileModel index={props.index} position={props.position}/>
         </mesh>
     )
